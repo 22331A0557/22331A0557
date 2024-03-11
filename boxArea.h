408fd99bd7547b0a4ac23d6b04075e6ca5305c54
Sasi Kumar <159427656+SasiKumar01430@users.noreply.github.com>
2024-03-11 15:53:21 +0530

    Update boxArea.h

--- boxArea.h
+++ boxArea.h
@@ -5,12 +5,4 @@ void boxArea(float length,float width,float height)
     float area = 2*(length*width+width*height+height*length);
     cout << "Area of the box is " << area << endl;
 }
-boxVolume.h
-#include<iostream>
-using namespace std;
-void boxVolume(float length,float width,float height)
 
-{
-    float vol = length*width*height;
-    cout << "Volume of the box is " << vol << endl;
-}
