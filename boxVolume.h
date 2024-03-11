0b965e8d335eea8b18eb6ebb5471ac4f6eea6f47
Sasi Kumar <159427656+SasiKumar01430@users.noreply.github.com>
2024-03-11 15:54:57 +0530

    Update boxVolume.h

--- boxVolume.h
+++ boxVolume.h
@@ -1,7 +1,8 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-#define BOX_VOLUME_H
+void boxVolume(float length,float width,float height)
 
-void boxVolume(float length, float width, float height){
-    cout << "Box Volume : " << length*width*height << endl;
+{
+    float vol = length*width*height;
+    cout << "Volume of the box is " << vol << endl;
 }
