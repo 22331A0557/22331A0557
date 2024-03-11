df1eac0e493b6c13b4fa148bcabf15cbdf9904e0
Sasi Kumar <159427656+SasiKumar01430@users.noreply.github.com>
2024-03-11 16:09:35 +0530

    Update constructor.cpp

--- constructor.cpp
+++ constructor.cpp
@@ -1,45 +1,66 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Student
-{
-public:
-    string fullName, collegeName;
-    int collegeCode;
-    double semPercentage;
-    // Default constructor
-    Student()
-    {
-        collegeName = "MVGR";
-        collegeCode = 33;
-    }
 
-    // Parameterized constructor
-    Student(Student &obj)
-    {
-        cout << "Enter name : ";
-        getline(cin, obj.fullName);
-        cout << "Enter semPercentage : ";
-        cin >> obj.semPercentage;
-    }
-    ~Student()
-    {
-        cout << "DEAD" << endl;
-    }
+inline void msg() { cout << "Welcome to C++" << endl ;}
 
-    void display()
-    {
-        cout << "Full Name : " << fullName << endl;
-        cout << "Sem Percentage : " << semPercentage << endl;
-        cout << "College Name : " << collegeName << endl;
-        cout << "College Code : " << collegeCode << endl;
-    }
+
+class student{
+    private:
+        string fullname;
+        int rollnum;
+        double sempercentage;
+        string collegename;
+        int collegecode;
+    public:
+        student(){}
+        student(string name,int roll,string college,int code,double percentage)
+        {
+            fullname = name;
+            rollnum = roll;
+            collegename = college;
+            collegecode = code;
+            sempercentage = percentage;
+
+
+        }
+        ~student(){
+            cout << "I am dead" <<endl;
+        }
+        void printstudentdetails()
+        {
+            cout << "The student details : " << endl;
+            cout << "Name : " << fullname << endl
+            << "Rollno : " << rollnum << endl
+            << "College : " << collegename << endl
+            << "College code : " << collegecode << endl
+            << "Sem percentage : " << sempercentage   << endl;
+        }     
 };
+
 
 int main()
 {
-    Student student1;
-    Student student2 = student1;
-    cout << "-------------------Student-1 Details----------------------" << endl;
-    student1.display();
+    string name;
+    int roll;
+    double semper;
+    string collname;
+    int collcode;
+    cout << "Enter your Full name : " ;
+    getline(cin,name);
+    cout << "Enter your Roll no : " ;
+    cin >> roll;
+    cout << "Enter your College name : " <<ends;
+    cin >> collname;
+    cout << "Enter your College code : " ;
+    cin >> collcode;
+    cout << "Enter your Sem percentage : " ;
+    cin >> semper;
+
+
+    student s(name,roll,collname,collcode,semper);
+
+
+    s.printstudentdetails();
+   
 }
